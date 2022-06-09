cde680e72969dabe4e3927f8b43f5199b7f821de
Steadystudy <sgmin206@naver.com>
2022-06-10 00:51:21 +0900

    Feat/ index로 모든 API Call 내보내기

---
+++ src/apis/index.js
@@ -0,0 +1,28 @@
+export { login, signUp, logOut, getAuthUser } from './auth';
+export {
+  getAllUsers,
+  getOnlineUsers,
+  getSpecificUser,
+  postProfileImg,
+  postCoverImg,
+} from './user';
+export { putPassword, putInfo } from './settings';
+export { getChannels, getSpecificChannel } from './channels';
+export {
+  getChannelPosts,
+  getAuthorPosts,
+  createPosts,
+  getPost,
+  putPost,
+  deletePost,
+} from './posts';
+export { postLike, deleteLike } from './like';
+export { postComments, deleteComments } from './comments';
+export {
+  getNotifications,
+  getNotificationsSeen,
+  postNotifications,
+} from './notifications';
+export { postFollow, deleteFollow } from './follow';
+export { getMessages, getConversations, postMessages } from './messages';
+export { searchUser, searchAll } from './search';
