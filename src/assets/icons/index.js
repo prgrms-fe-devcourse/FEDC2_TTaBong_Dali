7922cdce39ecd8ba25bdd48b3e350b1f23cfe99e
devBuzz142 <devbuzz142@gmail.com>
2022-06-12 21:33:06 +0900

    REFACTOR/ 아이콘 위치 이동 및 아이콘 index로 IconList 구현

---
+++ src/assets/icons/index.js
@@ -0,0 +1,15 @@
+import listFill from './icon_list_fill.svg';
+import rankingLine from './icon_ranking_line.svg';
+import searchLine from './icon_search_line.svg';
+import userLine from './icon_user_line.svg';
+import TTaBong from './TTaBong_white.svg';
+
+const IconList = {
+  listFill,
+  rankingLine,
+  searchLine,
+  userLine,
+  TTaBong,
+};
+
+export default IconList;
