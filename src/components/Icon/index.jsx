4fa7fff5236a407754dd164e4049064010243ccc
Owall <97934878+MayOwall@users.noreply.github.com>
2022-06-13 01:57:44 +0900

    Refactor/Icon 컴포넌트의 props, jsx 및 propTypes  수정

--- src/components/Icon/index.jsx
+++ src/components/Icon/index.jsx
@@ -3,32 +3,18 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import IconList from '../../assets/icons';
 
-const Icon = ({
-  icon = 'ranking',
-  type = 'Line',
-  size = 20,
-  alt = 'default-icon',
-}) => {
+const Icon = ({ name = 'listFill', size = 20 }) => {
   const iconStyle = {
     height: typeof size === 'string' ? size : `${(size / 8).toFixed(2)}rem`,
     width: typeof size === 'string' ? size : `${(size / 8).toFixed(2)}rem`,
   };
 
-  const src =
-    icon === 'TTaBong' ? IconList.TTaBong : IconList[`${icon}${type}`];
-
-  return (
-    <div>
-      <img src={src} alt={alt} style={{ ...iconStyle }} />
-    </div>
-  );
+  return <img src={IconList[name]} style={{ ...iconStyle }} alt={name} />;
 };
 
 Icon.propTypes = {
-  icon: PropTypes.string,
-  type: PropTypes.string,
+  name: PropTypes.string.isRequired,
   size: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
-  alt: PropTypes.string,
 };
 
 export default Icon;
