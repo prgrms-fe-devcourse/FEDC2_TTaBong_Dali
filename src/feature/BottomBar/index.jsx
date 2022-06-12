f2005aa7bef6c52d775ef40cdc5e8fea0fa9b4ae
devBuzz142 <devbuzz142@gmail.com>
2022-06-12 22:30:03 +0900

    REFACTOR/BottomBar page를 props로 받도록 리팩토링 및 스토리 생성

--- src/feature/BottomBar/index.jsx
+++ src/feature/BottomBar/index.jsx
@@ -1,30 +1,72 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import * as S from './style';
 import Icon from '../../components/Icon';
-import PropTypes from 'prop-types';
 
+const BottomBar = ({ page = 'mainFeed' }) => {
+  const pageIconList = {
+    mainFeed: [
+      { icon: 'list', type: 'Fill', alt: 'list-Fill' },
+      { icon: 'ranking', type: 'Line', alt: 'ranking-Line' },
+      { icon: 'search', type: 'Line', alt: 'search-Line' },
+      { icon: 'user', type: 'Line', alt: 'user-Line' },
+    ],
+    ranking: [
+      { icon: 'list', type: 'Line', alt: 'list-Line' },
+      { icon: 'ranking', type: 'Fill', alt: 'ranking-Fill' },
+      { icon: 'search', type: 'Line', alt: 'search-Line' },
+      { icon: 'user', type: 'Line', alt: 'user-Line' },
+    ],
+    search: [
+      { icon: 'list', type: 'Line', alt: 'list-fill' },
+      { icon: 'ranking', type: 'Line', alt: 'ranking-Line' },
+      { icon: 'search', type: 'Fill', alt: 'search-Fill' },
+      { icon: 'user', type: 'Line', alt: 'user-Line' },
+    ],
+    user: [
+      { icon: 'list', type: 'Line', alt: 'list-Line' },
+      { icon: 'ranking', type: 'Line', alt: 'ranking-Line' },
+      { icon: 'search', type: 'Line', alt: 'search-Line' },
+      { icon: 'user', type: 'Fill', alt: 'user-Fill' },
+    ],
+  };
 
-const BottomBar = ({ iconList, TTaBongIcon }) => {
+  const iconList = pageIconList[page];
+
   return (
     <S.BottomBarContainer>
       <S.IconContainer>
-        <Icon src={iconList[0]} />
-        <Icon src={iconList[1]} />
+        <Icon
+          icon={iconList[0].icon}
+          type={iconList[0].type}
+          alt={iconList[0].alt}
+        />
+        <Icon
+          icon={iconList[1].icon}
+          type={iconList[1].type}
+          alt={iconList[1].alt}
+        />
       </S.IconContainer>
       <S.TTaBongContainerBox>
-        <Icon src={TTaBongIcon} />
+        <Icon icon="TTaBong" type="" alt="TTaBong" />
       </S.TTaBongContainerBox>
       <S.IconContainer>
-        <Icon src={iconList[2]} />
-        <Icon src={iconList[3]} />
+        <Icon
+          icon={iconList[2].icon}
+          type={iconList[2].type}
+          alt={iconList[2].alt}
+        />
+        <Icon
+          icon={iconList[3].icon}
+          type={iconList[3].type}
+          alt={iconList[3].alt}
+        />
       </S.IconContainer>
     </S.BottomBarContainer>
   );
 };
-
 BottomBar.propTypes = {
-  iconList: PropTypes.arrayOf(PropTypes.string),
-  TTaBongIcon: PropTypes.string
+  page: PropTypes.string.isRequired,
 };
 
 export default BottomBar;
