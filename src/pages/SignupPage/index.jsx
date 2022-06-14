efaa5b59168afa44a71889af890b6783df49db7d
devBuzz142 <devbuzz142@gmail.com>
2022-06-14 15:19:57 +0900

    FEAT/pages 프로토타입 구현

---
+++ src/pages/SignupPage/index.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const SignupPage = () => {
+  return <div>Signup Page</div>;
+};
+
+export default SignupPage;
---
+++ src/pages/UserProfilePage/index.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const UserProfilePage = () => {
+  return <div>UserProfile Page</div>;
+};
+
+export default UserProfilePage;
