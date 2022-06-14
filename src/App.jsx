0c390e4e76e0e5382c59f7a648585ee18780a30d
devBuzz142 <devbuzz142@gmail.com>
2022-06-14 15:11:36 +0900

    FEAT/Router, Routes 기초 설정... 프로토타입

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,24 @@
 import React from 'react';
 import { ThemeProvider } from '@emotion/react';
+import { Route, Routes, Navigate } from 'react-router-dom';
 import GlobalStyle from './commons/style/GlobalStyle';
 import theme from './commons/style/themes';
+import NotFoundPage from './pages/NotFound';
+import BottomBar from './feature/BottomBar';
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyle />
-      <div>Test</div>
+      <Routes>
+        <Route path="/main" element={<BottomBar page="mainfeed" />} />
+        <Route path="/cardDetail" element={<NotFoundPage />} />
+        <Route path="/ranking" element={<NotFoundPage />} />
+        <Route path="/TTaBong" element={<NotFoundPage />} />
+        <Route path="/search" element={<NotFoundPage />} />
+        <Route path="/userProfile" element={<NotFoundPage />} />
+        <Route path="/*" element={<BottomBar />} />
+      </Routes>
     </ThemeProvider>
   );
 }
