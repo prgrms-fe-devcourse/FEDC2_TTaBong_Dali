6e8fa8c7c3809379b6c596ce5bc50a548e12610b
Owall <97934878+MayOwall@users.noreply.github.com>
2022-06-15 03:50:07 +0900

    Fix/Router 태그로 컴포넌트를 감싸줌

--- src/feature/BottomBar/index.jsx
+++ src/feature/BottomBar/index.jsx
@@ -1,47 +1,49 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Link } from 'react-router-dom';
+import { BrowserRouter as Router, Link } from 'react-router-dom';
 import * as S from './style';
 import Icon from '../../components/Icon';
 
 // page props로 'mainFeed', 'rank', 'search', 'user'가 들어온다고 가정하겠습니다.
 const BottomBar = ({ page }) => {
   return (
-    <S.BottomBarContainer>
-      <S.IconContainer>
-        <Link to="/main">
-          <Icon
-            name={page === 'mainFeed' ? 'listFill' : 'listLine'}
-            alt="리스트 아이콘"
-          />
-        </Link>
-        <Link to="/rank">
-          <Icon
-            name={page === 'rank' ? 'rankFill' : 'rankLine'}
-            alt="랭킹 아이콘"
-          />
-        </Link>
-      </S.IconContainer>
-      <S.TTaBongContainerBox>
-        <Link to="/TTaBong">
-          <Icon name="TTaBongWhite" alt="따봉 아이콘" />
-        </Link>
-      </S.TTaBongContainerBox>
-      <S.IconContainer>
-        <Link to="/search">
-          <Icon
-            name={page === 'search' ? 'searchFill' : 'searchLine'}
-            alt="검색 아이콘"
-          />
-        </Link>
-        <Link to="/userProfile">
-          <Icon
-            name={page === 'user' ? 'userFill' : 'userLine'}
-            alt="유저 아이콘"
-          />
-        </Link>
-      </S.IconContainer>
-    </S.BottomBarContainer>
+    <Router>
+      <S.BottomBarContainer>
+        <S.IconContainer>
+          <Link to="/main">
+            <Icon
+              name={page === 'mainFeed' ? 'listFill' : 'listLine'}
+              alt="리스트 아이콘"
+            />
+          </Link>
+          <Link to="/rank">
+            <Icon
+              name={page === 'rank' ? 'rankFill' : 'rankLine'}
+              alt="랭킹 아이콘"
+            />
+          </Link>
+        </S.IconContainer>
+        <S.TTaBongContainerBox>
+          <Link to="/TTaBong">
+            <Icon name="TTaBongWhite" alt="따봉 아이콘" />
+          </Link>
+        </S.TTaBongContainerBox>
+        <S.IconContainer>
+          <Link to="/search">
+            <Icon
+              name={page === 'search' ? 'searchFill' : 'searchLine'}
+              alt="검색 아이콘"
+            />
+          </Link>
+          <Link to="/userProfile">
+            <Icon
+              name={page === 'user' ? 'userFill' : 'userLine'}
+              alt="유저 아이콘"
+            />
+          </Link>
+        </S.IconContainer>
+      </S.BottomBarContainer>
+    </Router>
   );
 };
 BottomBar.propTypes = {
