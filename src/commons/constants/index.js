2633c25b4dc67fe9ef207f66a4030e3281b3da4f
devBuzz142 <devbuzz142@gmail.com>
2022-06-14 23:54:59 +0900

    FEAT/Constants 구조 수정

--- src/commons/constants/index.js
+++ src/commons/constants/index.js
@@ -1,12 +1,25 @@
 const TTaBong = 'TTaBong';
 const BigTTaBong = 'BigTTaBong';
+
 const LabelGood = 'LabelGood';
 const LabelThanks = 'LabelThank';
+
+const OpacityVisible = 1;
+const OpacityTransparent = 0.7;
+
 const Constants = {
-  TTaBong,
-  BigTTaBong,
-  LabelGood,
-  LabelThanks,
+  TTaBong: {
+    TTaBong,
+    BigTTaBong,
+  },
+  Label: {
+    LabelGood,
+    LabelThanks,
+  },
+  Opacity: {
+    OpacityVisible,
+    OpacityTransparent,
+  },
 };
 
 export default Constants;
