48e6a4dcf45f4a104fcf9ede4459e120532d463d
devBuzz142 <devbuzz142@gmail.com>
2022-06-16 02:07:14 +0900

    FEAT/getHexToRgba 유틸함수 추가 및 적용

--- src/components/BaseContentContainer/style.jsx
+++ src/components/BaseContentContainer/style.jsx
@@ -1,12 +1,14 @@
 import styled from '@emotion/styled';
+import { getHexToRgb } from '../../utils/getHexToRgb';
 
 export const Container = styled.div`
   width: 95%;
   min-height: 100%;
   height: 100%;
   padding: 2rem 1rem 2rem 1rem;
+  background-color: ${(props) =>
+    `rgba(${getHexToRgb(props.theme.colors.white)},${props.opacity})`};
 
-  background-color: rgba(255, 255, 255, ${(props) => props.opacity});
   border-radius: 30px;
   box-shadow: 0px 4px 10px rgba(0, 0, 0, 0.25);
 `;
