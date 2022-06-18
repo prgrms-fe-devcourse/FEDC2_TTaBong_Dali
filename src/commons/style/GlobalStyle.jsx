b6f2c9fa8b6d02b85d6eb7ee4d40c5ed9f8fe0a2
woojerry <woojerry@naver.com>
2022-06-18 23:52:03 +0900

    Style/ GlobalFont 적용

--- src/commons/style/GlobalStyle.jsx
+++ src/commons/style/GlobalStyle.jsx
@@ -11,6 +11,10 @@ function GlobalStyle() {
           box-sizing: border-box;
           -moz-osx-font-smoothing: grayscale;
           -webkit-font-smoothing: antialiased;
+          font-family: -apple-system, BlinkMacSystemFont, Bazier Square,
+            Noto Sans KR, Segoe UI, Apple SD Gothic Neo, Roboto, Helvetica Neue,
+            Arial, sans-serif, Apple Color Emoji, Segoe UI Emoji,
+            Segoe UI Symbol, Noto Color Emoji;
         }
         input {
           -webkit-appearance: none;
