fda0398d5c16eeedc8925afb769600fdc7625827
Steadystudy <sgmin206@naver.com>
2022-06-19 17:10:34 +0900

    Fix/ 카운트 잘못된 부분 수정

--- src/pages/RankPage/index.jsx
+++ src/pages/RankPage/index.jsx
@@ -53,7 +53,7 @@ const RankPage = () => {
       for (let i = 0; i < allUserInfo.length; i += 1) {
         const { _id, coinCount } = allUserInfo[i];
         if (receiver === _id) {
-          const count = type === TTABONG ? coinCount + 1 : coinCount + 2;
+          const count = type === 'TTaBong' ? coinCount + 1 : coinCount + 2;
           allUserInfo[i].coinCount = count;
           break;
         }
