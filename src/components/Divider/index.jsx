bdd07cab9339682fa4a5422b0755810e7eff22ec
Owall <97934878+MayOwall@users.noreply.github.com>
2022-06-19 18:33:25 +0900

    Feat/Divider가 ...props를 받을 수 있도록 추가

--- src/components/Divider/index.jsx
+++ src/components/Divider/index.jsx
@@ -3,8 +3,8 @@ import PropTypes from 'prop-types';
 import * as S from './style';
 import theme from '../../commons/style/themes';
 
-const Divider = ({ size = 300, color = theme.colors.gray[0] }) => {
-  return <S.DivisionLine size={size} color={color} />;
+const Divider = ({ size = 300, color = theme.colors.gray[0], ...props }) => {
+  return <S.DivisionLine size={size} color={color} {...props} />;
 };
 
 Divider.propTypes = {
