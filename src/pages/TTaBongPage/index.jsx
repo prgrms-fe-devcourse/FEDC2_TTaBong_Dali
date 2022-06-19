30224724bb9b47d2fb4024ac75471890af8dc1ed
Steadystudy <sgmin206@naver.com>
2022-06-19 16:02:18 +0900

    Feat/ 따봉 페이지 프로토타입

--- src/pages/TTaBongPage/index.jsx
+++ src/pages/TTaBongPage/index.jsx
@@ -1,7 +1,14 @@
 import React from 'react';
+import PageTemplate from '../../feature/pageTemplate/PageTemplate';
+import BaseCardContainer from '../../components/BaseCardContainer';
 
 const TTaBongPage = () => {
-  return <div>TTaBong Page</div>;
+  return (
+    <PageTemplate>
+      {/* rem으로 넣음 */}
+      <BaseCardContainer height={37} />
+    </PageTemplate>
+  );
 };
 
 export default TTaBongPage;
