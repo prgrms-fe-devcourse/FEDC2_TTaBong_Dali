5bf055a5c751319139948407d01fd11e23709443
blacktoast <dbstjd100@naver.com>
2022-06-20 23:01:15 +0900

    Fix/ 음수버그수정

--- src/utils/getDayString.js
+++ src/utils/getDayString.js
@@ -4,7 +4,7 @@ const DATE_ONE_DAY = DATE_ONE_HOUR * 24;
 export const getDayString = (diffTime, createdTime) => {
   let dayString = '';
   if (diffTime < DATE_ONE_DAY) {
-    const countHours = Math.floor(diffTime / DATE_ONE_HOUR);
+    const countHours = Math.floor(diffTime < 0 ? 0 : diffTime / DATE_ONE_HOUR);
     dayString += `${countHours} 시간`;
   } else {
     const date = new Date(createdTime);
