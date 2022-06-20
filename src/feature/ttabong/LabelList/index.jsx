c6857758baf4240da611ba0f5c43250e4861cb53
devBuzz142 <devbuzz142@gmail.com>
2022-06-20 23:52:24 +0900

    Feat/ 칭찬사유, 이미지 업로드, 라벨 기능 부착

--- src/feature/ttabong/LabelList/index.jsx
+++ src/feature/ttabong/LabelList/index.jsx
@@ -3,35 +3,22 @@ import { type } from '@testing-library/user-event/dist/type';
 import * as S from './style';
 import Label from '../../../components/Label';
 
-const LabelList = () => {
-  const [praise, setPraise] = useState('');
-  const [moved, setMoved] = useState('');
-  const [warm, setWarm] = useState('');
-
+const LabelList = ({ labelItems, setLabelItems }) => {
   const onClickLabel = (type) => {
-    switch (type) {
-      case 'praise':
-        return praise === '' ? setPraise('praise') : setPraise('');
-      case 'moved':
-        return moved === '' ? setMoved('moved') : setMoved('');
-      case 'warm':
-        return warm === '' ? setWarm('warm') : setWarm('');
-      default:
-        return null;
-    }
+    setLabelItems({ ...labelItems, [type]: labelItems[type] ? '' : type });
   };
 
   return (
     <S.StyledLabelList>
       <S.LabelTitle>라벨</S.LabelTitle>
       <S.LabelContent>
-        <Label onClick={() => onClickLabel('praise')} type={praise}>
+        <Label onClick={() => onClickLabel('praise')} type={labelItems.praise}>
           칭찬
         </Label>
-        <Label onClick={() => onClickLabel('moved')} type={moved}>
+        <Label onClick={() => onClickLabel('moved')} type={labelItems.moved}>
           감동
         </Label>
-        <Label onClick={() => onClickLabel('warm')} type={warm}>
+        <Label onClick={() => onClickLabel('warm')} type={labelItems.warm}>
           따뜻
         </Label>
       </S.LabelContent>
