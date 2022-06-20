de831210276ae686bb8c48d09f7936e14de8fdf9
devBuzz142 <devbuzz142@gmail.com>
2022-06-20 22:05:29 +0900

    Feat/따봉 페이지 프로토타입

--- src/pages/TTaBongPage/index.jsx
+++ src/pages/TTaBongPage/index.jsx
@@ -1,7 +1,19 @@
 import React from 'react';
+import PageTemplate from '../../feature/pageTemplate/PageTemplate';
+import * as S from './style';
 
 const TTaBongPage = () => {
-  return <div>TTaBong Page</div>;
+  return (
+    <PageTemplate>
+      <S.PraisePageContainer>
+        <S.BePraisedContainer>bePraised</S.BePraisedContainer>
+        <S.PraiseReasonContainer>reason</S.PraiseReasonContainer>
+        <S.ImageUploaderContainer>uploader</S.ImageUploaderContainer>
+        <S.LabelListContainer>labels</S.LabelListContainer>
+        <S.PraiseButtonContainer>button</S.PraiseButtonContainer>
+      </S.PraisePageContainer>
+    </PageTemplate>
+  );
 };
 
 export default TTaBongPage;
---
+++ src/pages/TTaBongPage/style.jsx
@@ -0,0 +1,12 @@
+import styled from '@emotion/styled';
+
+export const PraisePageContainer = styled.div`
+  width: 100%;
+  height: 100%;
+`;
+
+export const BePraisedContainer = styled.div``;
+export const PraiseReasonContainer = styled.div``;
+export const ImageUploaderContainer = styled.div``;
+export const LabelListContainer = styled.div``;
+export const PraiseButtonContainer = styled.div``;
