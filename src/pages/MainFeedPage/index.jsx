f8808255f463a12f103766d615b0a1c71b344acd
Owall <97934878+MayOwall@users.noreply.github.com>
2022-06-20 16:40:36 +0900

    Feat/드디어..git add . 무한스크롤 구현

--- src/pages/MainFeedPage/index.jsx
+++ src/pages/MainFeedPage/index.jsx
@@ -12,40 +12,62 @@ import { useScrollDown } from '../../hooks/useScrollDown';
 const MainFeedPage = () => {
   const { Posts } = DummyData;
   const [posts, setPosts] = useState([]);
-  const [lastPost, setLastPost] = useState({});
+  const [target, setTarget] = useState(null);
   const [loading, setLoading] = useState(false);
   const [offset, setOffset] = useState(0);
 
   const [ref, isScrollDown] = useScrollDown();
-  const [last, inView] = useInView();
 
   const CHANNEL_ID = '62a19123d1b81239d875d20d';
 
-  // inView값이 변경될 때 마다 작동해야하는 useEffect
-  // 어째서인지 작동이 안되고 있다.....젭알..왜 안되...
+  const getNextPosts = () => {
+    setLoading(true);
+    console.log('start loading');
+    getChannelPosts(CHANNEL_ID, offset, 5)
+      .then((response) => {
+        if (!response) return null;
+        console.log(response);
+        const newPosts = [...posts, ...response];
+        console.log('post + response', newPosts);
+        setPosts(newPosts || Posts);
+      })
+      .then(() => {
+        setOffset(offset + 5);
+        console.log('setOffset : ', offset);
+      });
+    setLoading(false);
+    console.log('end loading');
+  };
+
+  const onIntersect = async (entries, observer) => {
+    console.log('안녕하신가');
+    console.log('posts.length, offset', posts.length, offset);
+    if (posts.length !== offset) return;
+    entries.forEach((entry) => {
+      if (entry.isIntersecting && !loading) {
+        observer.unobserve(entry.target);
+        getNextPosts();
+        observer.observe(entry.target);
+      }
+    });
+  };
+
   useEffect(() => {
-    console.log('scroll ended');
-    if (inView) {
-      console.log('scroll is end!');
+    let observer;
+    if (target) {
+      observer = new IntersectionObserver(onIntersect, {
+        threshold: 0.3,
+      });
+      observer.observe(target);
+      console.log('observe is done');
     }
-  }, [inView]);
+    return () => observer && observer.disconnect();
+  }, [target]);
 
   // 최초 랜더링
   useEffect(() => {
-    const getPosts = async () => {
-      setLoading(true);
-      console.log('start loading');
-
-      await getChannelPosts(CHANNEL_ID, offset).then((response) => {
-        setPosts(response || Posts);
-        console.log('response : ', response);
-      });
-      // 왜 로딩은 성공적으로 되었는데 post는 제대로 나오지 않는가....
-      console.log('posts : ', posts);
-      console.log('loaded posts successfully');
-
-      setLoading(false);
-      console.log('finish loading');
+    const getPosts = () => {
+      getNextPosts();
     };
     getPosts();
   }, []);
@@ -61,9 +83,8 @@ const MainFeedPage = () => {
         {posts.map((post, idx) => (
           <S.MainCardWrapper key={post._id}>
             {posts.length - 1 === idx ? (
-              <div>
-                <MainCard post={post} ref={last} />
-                <div>hello</div>
+              <div ref={setTarget}>
+                <MainCard post={post} />
               </div>
             ) : (
               <MainCard post={post} />
