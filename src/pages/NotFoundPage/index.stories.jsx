5b6a0f05c70a7d25490793f1c030acb2a3dcae4f
woojerry <woojerry@naver.com>
2022-06-21 11:01:27 +0900

    Chore/ 불필요한 코드 제거

--- src/pages/NotFoundPage/index.stories.jsx
+++ src/pages/NotFoundPage/index.stories.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import NotFoundPage from './index';
-import { notFound } from '../../assets/images/notfound.png';
 
 export default {
   title: 'Pages/NotFoundPage',
