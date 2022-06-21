e4b9f4640b2e84069b6603174d7b6d6b6752de0e
Owall <97934878+MayOwall@users.noreply.github.com>
2022-06-22 01:36:58 +0900

    Refactor/프로필 편집 페이지 라우팅 수정

--- src/pages/ProfileEditPage/index.jsx
+++ src/pages/ProfileEditPage/index.jsx
@@ -3,15 +3,17 @@ import * as S from './style';
 import PageTemplate from '../../feature/pageTemplate/PageTemplate';
 import { putPassword } from '../../apis/settings';
 import { PASSWORD_UPDATE_SUCCESS } from '../../commons/constants/string';
+import { useAuthContext } from '../../contexts/UserProvider';
 
 const ProfileEditPage = () => {
   const navigate = useNavigate();
+  const { authUser } = useAuthContext();
 
   const handleChangePassword = async (token, password) => {
     const response = await putPassword(token, password);
     if (response === 'Password updated successfully.') {
       alert(PASSWORD_UPDATE_SUCCESS);
-      navigate('/userProfile'); // myUserProfile로 해야할듯
+      navigate(`/userProfile/${authUser.userId}`); // myUserProfile로 해야할듯
     }
   };
 
