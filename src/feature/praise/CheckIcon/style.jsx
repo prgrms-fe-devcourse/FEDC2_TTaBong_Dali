87bcf8fa6f02611003d77eff6812c0ab553c69c3
devBuzz142 <devbuzz142@gmail.com>
2022-06-21 13:59:40 +0900

    Fix/ 컨벤션 수정 및 useEffect async 제거

--- src/feature/praise/CheckIcon/style.jsx
+++ src/feature/praise/CheckIcon/style.jsx
@@ -1,19 +1,20 @@
 import styled from '@emotion/styled';
 import theme from '../../../commons/style/themes';
+import { getPxToRem } from '../../../utils/getPxToRem';
 
 export const CheckIconWrapper = styled.div``;
 
 export const UncheckedBox = styled.div`
-  width: 24px;
-  height: 24px;
+  width: ${getPxToRem(24)};
+  height: ${getPxToRem(24)};
 
   border: 1px solid ${theme.colors.gray[0]};
   border-radius: 50%;
 `;
 
 export const CheckedBox = styled.div`
-  width: 24px;
-  height: 24px;
+  width: ${getPxToRem(24)};
+  height: ${getPxToRem(24)};
 
   border: 2px solid ${theme.colors.yellowHover};
   border-radius: 50%;
