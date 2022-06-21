d1bbe08aefc43dbf82a5044a24dd320a2da3ca17
woojerry <woojerry@naver.com>
2022-06-21 14:51:12 +0900

    Feat/ RegisterForm 비밀번호 불일치 에러 확인 추가

--- src/commons/constants/error.js
+++ src/commons/constants/error.js
@@ -2,4 +2,4 @@ export const USERNAME_ISNILL_ERRROR = '이름을 입력해주세요.';
 export const ID_ISNILL_ERROR = '아이디를 입력해주세요.';
 export const PASSWORD_ISNILL_ERROR = '비밀번호를 입력해주세요.';
 export const PASSWORDCONFIRM_ISNILL_ERROR = '비밀번호 확인을 입력해주세요.';
-export const PASSWORD_UNEQUAL_ERROR = '비밀번호를 확인해주세요.';
+export const PASSWORD_UNEQUAL_ERROR = '비밀번호가 같은지 확인해주세요.';
