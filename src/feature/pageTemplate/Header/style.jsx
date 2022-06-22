eae574a0f129f36d873efd1bccf2df0a98159dbe
Owall <97934878+MayOwall@users.noreply.github.com>
2022-06-22 12:35:46 +0900

    Style/헤더 스타일 수정

--- src/feature/pageTemplate/Header/style.jsx
+++ src/feature/pageTemplate/Header/style.jsx
@@ -2,29 +2,31 @@ import styled from '@emotion/styled';
 import Logo from '../../../components/Logo';
 
 export const Header = styled.div`
-  position: fixed;
-  left: 0%;
-  right: 0%;
-  top: 0%;
-  height: 58px;
   z-index: 3000;
-  display: flex;
-  justify-content: space-between;
-  transition: border-radius 0.3s;
-  background-color: ${({ theme }) => theme.colors.skyblue[0]};
-  border-radius: 0px 0px 30px 30px;
-`;
-
-export const AvatarContainer = styled.div`
+  position: fixed;
+  left: 0;
+  right: 0;
+  top: 0;
+  height: 3.6rem;
   display: flex;
   justify-content: center;
   align-items: center;
-  margin-left: 16px;
-  gap: 16px;
+  transition: border-radius 0.3s;
+  background-color: ${({ theme }) => theme.colors.skyblue[0]};
+  border-radius: 0 0 2rem 2rem;
+`;
+
+export const AvatarContainer = styled.div`
+  position: fixed;
+  left: 1.5rem;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 1rem;
 `;
 
 export const TTaBongKingAvatarWrapper = styled.div`
-  border: 3px solid transparent;
+  border: 0.2rem solid transparent;
   border-radius: 50%;
   background-image: linear-gradient(#fff, #fff),
     linear-gradient(
@@ -37,7 +39,7 @@ export const TTaBongKingAvatarWrapper = styled.div`
 `;
 
 export const CoinKingAvatarWrapper = styled.div`
-  border: 3px solid transparent;
+  border: 0.2rem solid transparent;
   border-radius: 50%;
   background-image: linear-gradient(#fff, #fff),
     linear-gradient(
@@ -51,13 +53,15 @@ export const CoinKingAvatarWrapper = styled.div`
 
 export const PlacedLogo = styled(Logo)`
   cursor: pointer;
-  margin-right: 8px;
+  margin-top: 0.2rem;
 `;
 
 export const IconWrapper = styled.div`
   cursor: pointer;
   display: flex;
   align-items: center;
-  margin-right: 16px;
   justify-content: center;
+  position: fixed;
+  top: 0.6rem;
+  right: 1.5rem;
 `;
