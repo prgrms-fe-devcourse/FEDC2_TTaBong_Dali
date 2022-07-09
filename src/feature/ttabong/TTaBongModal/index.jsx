2a0c71a0d1925070dffe1ce6038296f842cc2108
Steadystudy <sgmin206@naver.com>
2022-07-09 14:28:42 +0900

    Modify/ 검색 기능 수정 startsWith => indexOf

--- src/feature/ttabong/TTaBongModal/index.jsx
+++ src/feature/ttabong/TTaBongModal/index.jsx
@@ -1,16 +1,13 @@
-import React, { useState, useEffect } from 'react';
-import PropTypes from 'prop-types';
+import React, { useState, useEffect, useCallback } from 'react';
 import * as S from './style';
-import Modal, { useModal } from '../../../components/Modal';
+import Modal from '../../../components/Modal';
 import BaseCardContainer from '../../../components/BaseCardContainer';
-import InputForm from '../../../components/InputForm';
 import UserInfoItem from '../../../components/UserInfoItem';
 import theme from '../../../commons/style/themes';
-import useForm from '../../../hooks/useForm';
-import { getAllUsers, searchUser } from '../../../apis';
+import { getAllUsers } from '../../../apis';
 import { useAuthContext } from '../../../contexts/UserProvider';
 
-const TTaBongModal = ({ checkedUsers, setCheckedUsers, modalProps }) => {
+const TTaBongModal = ({ setCheckedUsers, modalProps }) => {
   const { isModalOn, backgroundRef, handleCloseModal } = modalProps;
 
   const { authUser } = useAuthContext();
@@ -32,13 +29,11 @@ const TTaBongModal = ({ checkedUsers, setCheckedUsers, modalProps }) => {
   }, []);
 
   const handleSearchSubmit = (e) => {
-    e.preventDefault();
-
-    const keyword = e.target.search.value;
+    const keyword = e;
 
     setUsers(
       users.map((user) =>
-        user.fullName.startsWith(keyword)
+        user.fullName.indexOf(keyword) >= 0
           ? { ...user, searched: true }
           : { ...user, searched: false },
       ),
@@ -46,7 +41,7 @@ const TTaBongModal = ({ checkedUsers, setCheckedUsers, modalProps }) => {
   };
 
   const handleUserCheck = (e, userId) => {
-    e.preventDefault(); // 5명까지만
+    e.preventDefault();
 
     const newUsers = users.map((user) =>
       user._id !== userId ? user : { ...user, checked: !user.checked },
@@ -57,7 +52,6 @@ const TTaBongModal = ({ checkedUsers, setCheckedUsers, modalProps }) => {
     if (newCheckdUsers.length > 5) return;
 
     setUsers(newUsers);
-
     setCheckedUsers(newCheckdUsers);
   };
 
@@ -103,7 +97,5 @@ const TTaBongModal = ({ checkedUsers, setCheckedUsers, modalProps }) => {
     </Modal>
   );
 };
-
-TTaBongModal.propTypes = {};
 
 export default TTaBongModal;
